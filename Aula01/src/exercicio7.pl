9d4427d692365b3e25c4d5c5dbf773119deea910
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-11-17 11:28:01 +0000

    correcções exercicio 7

--- Aula01/src/exercicio7.pl
+++ Aula01/src/exercicio7.pl
@@ -1,7 +1,4 @@
 /*
-        Esta não é melhor forma de programar em prolog porque repete muitas vezes
-o mesmo predicado.
-
 traduza(Codigo, Significado):-
         Codigo = 1,
         Significado = integer_overflow.
@@ -14,14 +11,19 @@ traduza(Codigo, Significado):-
         Codigo = 3,   
         Significado = id_desconhecido.
 
-        Enquanto, que o exemplo a abaixo é mais efeciente porque no mesmo 
-predicado permite ter as mesmas soluções.
+        Funciona mas não está correcto, uma vez que apenas faz unificações e
+não comparações. Também não é capaz de procurar pelo atom 
 */
 
+traduza(1, integer_overflow).
 traduza(Codigo, Significado):-
-        Codigo = 1,
-        Significado = integer_overflow;
-        Codigo = 2,   
-        Significado = divisao_por_zero;   
-        Codigo = 3,   
+        Codigo == 1,
+        Significado = integer_overflow.
+traduza(2, divisao_por_zero).
+traduza(Codigo, Significado):-
+        Codigo == 2,   
+        Significado = divisao_por_zero.
+traduza(3, id_desconhecido).    
+traduza(Codigo, Significado):-
+        Codigo == 3,   
         Significado = id_desconhecido.
