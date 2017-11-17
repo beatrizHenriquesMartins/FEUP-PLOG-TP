6c15dff4637142cb833a6e1c72f483f557aaba64
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-11-17 12:28:34 +0000

    exercicio 1 - ficha 2

--- Aula02/src/exercicio1.pl
+++ Aula02/src/exercicio1.pl
@@ -1,10 +1,11 @@
-r(a,b).
-r(a,c).
-r(b,a).
-r(a,d).
-s(b,c).
-s(b,d).
-s(c,c).
-s(d,e).
+r(a, b).     
+r(a, c).     
+r(b, a).     
+r(a, d).
+s(b, c).     
+s(b, d).     
+s(c, c).     
+s(d, e).
 
-/* fazer a representação da arvore */
+/* fazer a representação da arvore, o resultado tem de ser true */
+ 
