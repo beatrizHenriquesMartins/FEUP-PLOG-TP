8c424001ddf0f32aac57bcdf8ff5ad890b7eb65a
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-11-17 12:17:45 +0000

    exercicio 10 - ficha 2 completa!!!

---
+++ Aula01/src/exercicio10.pl
@@ -0,0 +1,23 @@
+comprou(joao, honda).
+comprou(joao, uno).
+  
+ano(honda, 1997).
+ano(uno, 1998).  
+
+valor(honda, 20000).  
+valor(uno, 7000). 
+
+% a) Crie uma regra pode_vender onde o primeiro argumento é a pessoa,
+% o segundo o carro e o terceiro é o ano actual (não especificar 'homem' 
+% ou 'carro' nas regras), onde a pessoa só pode vender o carro se o carro
+% for comprado por ela nos últimos 10 anos e se seu valor for menor do que 
+% 10000 Euros. 
+
+pode_vender(Pessoa, Carro, Ano):-
+        comprou(Pessoa,Carro),
+        ano(Carro, Ano1),
+        Ano2 is Ano-Ano1,
+        Ano2 > 0,
+        Ano2 =< 10,
+        valor(Carro, Valor),
+        Valor < 10000.
