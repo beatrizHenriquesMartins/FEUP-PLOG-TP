d9f07ca707e02eb6f6277a7d3e1a2ce992cc2920
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-11-17 12:37:08 +0000

    exercicio 2 +/-

--- Aula02/src/exercicio2.pl
+++ Aula02/src/exercicio2.pl
@@ -1,10 +1,47 @@
 a(a1,1).
-a(A,2).
-a(a3,N).
+a('A',2).
+a(a3,'N').
 b(1,b1).
-b(2,B).
-b(N,b3).
+b(2,'B').
+b('N',b3).
 
-c(X,Y) :- a(X,N), b(N,Y).
-d(X,Y) :- a(X,N), b(Y,N).
-d(X,Y) :- a(N,X), b(N,Y).
+c(X,Y):- 
+        a(X,N), 
+        b(N,Y).
+
+d(X,Y):-
+        a(X,N), 
+        b(Y,N).
+
+d(X,Y):-
+        a(N,X), 
+        b(N,Y).
+
+/*
+*************
+* RESPOSTAS *
+*************
+
+a) |?- a(X,2).
+    X = 'A' ? ;
+    no
+
+b) |?- b(X,kalamazoo).
+    no
+
+c) |?- c(X,b3).
+    X = a3 ? ;
+    no
+
+d) |?- c(X,Y).
+    X = a1,
+    Y = b1 ? ;
+    X = 'A',
+    Y = 'B' ? ;
+    X = a3,
+    Y = b3 ? ;
+    no
+
+e) |?- d(X,Y).
+    no
+*/
