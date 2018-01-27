12607d148e1c728914b382d0811e7d433eb39a17
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-01-27 17:14:43 +0000

    pergunta 1 e 2

--- MiniTeste1_1718/src/grupo1.pl
+++ MiniTeste1_1718/src/grupo1.pl
@@ -4,19 +4,19 @@
 % * Base de factos *
 % ******************
  
-%player(Name, UserName, Age).
+% player(Name, UserName, Age).
 player('Danny', 'Best Player Ever', 27).
 player('Annie', 'Worst Player Ever', 24).
 player('Harry', 'A-Star Player', 26).
 player('Manny', 'The Player', 14).
 player('Jonny', 'A Player', 16).
 
-%game(Name, Categories, MinAge).
+% game(Name, Categories, MinAge).
 game('5 ATG', [action, adventure, open-world, multiplayer], 18).
 game('Carrier Shift: Game Over', [action, fps, multiplayer, shooter], 16).
 game('Duas Botas', [action, free, strategy, moba], 12).
 
-%played(Player, Game, HoursPlayed, PercentUnlocked)
+% played(Player, Game, HoursPlayed, PercentUnlocked)
 played('Best Player Ever', '5 ATG', 3, 83).
 played('Worst Player Ever', '5 ATG', 52, 9).
 played('The Player', 'Carrier Shift: Game Over', 44, 22).
@@ -35,6 +35,11 @@ playedALot(Player) :-
 % **************
 % * Pergunta 2 *
 % **************
+
+isAgeAppropriate(Name, Game) :-
+        player(Name, _, Age),
+        game(Game, _, MinAge),
+        Age >= MinAge.
 
 % **************
 % * Pergunta 3 *
