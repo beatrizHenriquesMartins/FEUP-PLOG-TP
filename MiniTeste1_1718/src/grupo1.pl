f02c2a42ed2aadf03881d356e70292a739141b0e
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2018-01-29 00:36:40 +0000

    pergunta 8

--- MiniTeste1_1718/src/grupo1.pl
+++ MiniTeste1_1718/src/grupo1.pl
@@ -25,26 +25,26 @@ played('A Player', 'Carrier Shift: Game Over', 48, 24).
 played('A-Star Player', 'Duas Botas', 37, 16).
 played('Best Player Ever', 'Duas Botas', 33, 22).
 
-% **************
-% * Pergunta 1 *
-% **************
+% *******************
+% * Pergunta 1 - V1 *
+% *******************
 
 playedALot(Player) :-
         played(Player, _, HoursPlayed, _),
         HoursPlayed >= 50.
 
-% **************
-% * Pergunta 2 *
-% **************
+% *******************
+% * Pergunta 2 - V1 *
+% *******************
 
 isAgeAppropriate(Name, Game) :-
         player(Name, _, Age),
         game(Game, _, MinAge),
         Age >= MinAge.
 
-% **************
-% * Pergunta 3 *
-% **************
+% *******************
+% * Pergunta 3 - V1 *
+% *******************
 
 listGamesOfCategory(Cat):-
         game(Game, Categories, MinAge),
@@ -52,9 +52,9 @@ listGamesOfCategory(Cat):-
         write(Game), write(' '), write('('), write(MinAge), write(')'), nl,
         fail;true.
 
-% **************
-% * Pergunta 4 *
-% **************
+% *******************
+% * Pergunta 4 - V1 *
+% *******************
 
 updatePlayer(Player, Game, Hours, Percentage) :-
         played(Player, Game, HoursPlayed, PercentageUnlocked),
@@ -63,9 +63,9 @@ updatePlayer(Player, Game, Hours, Percentage) :-
         retract(played(Player, Game, _, _)),
         assert(played(Player, Game, NewHours, NewPercentage)).
 
-% **************
-% * Pergunta 5 *
-% **************
+% *******************
+% * Pergunta 5 - V1 *
+% *******************
 
 timePlayingGames(_, [], [], 0).
 timePlayingGames(Player, [Game|Games], [Time|ListOfTimes], SumTimes) :-
@@ -76,22 +76,46 @@ timePlayingGames(Player, [Game|Games], [Time|ListOfTimes], SumTimes) :-
 timePlayingGames(Player, [_|Games], [0|ListOfTimes], SumTimes):-
         timePlayingGames(Player, Games, ListOfTimes, SumTimes).
 
-% **************
-% * Pergunta 6 *
-% **************
+% *******************
+% * Pergunta 6 - V1 *
+% *******************
 
-% **************
-% * Pergunta 7 *
-% **************
+littleAchievement(Player, Games):-
+        littleAchievement(Player, [], Games).
+littleAchievement(Player, ListOfGames, FinalGames):-
+        played(Player, GameName, _, PercentageUnlock),
+        PercentageUnlock < 20,
+        \+(member(GameName, ListOfGames)),
+        !,
+        littleAchievement(Player, [GameName|ListOfGames], FinalGames).
+% condição de paragem
+littleAchievement(Player, FinalGames, FinalGames).
 
-% **************
-% * Pergunta 8 *
-% **************
+% *******************
+% * Pergunta 7 - V1 *
+% *******************
 
-% **************
-% * Pergunta 9 *
-% **************
+ageRange(MinAge, MaxAge, Players):-
+        findall(PlayerName, (player(PlayerName, _, Age), Age >= MinAge, Age =< MaxAge), Players). 
 
-% ***************
-% * Pergunta 10 *
-% ***************
+% *******************
+% * Pergunta 8 - V1 *
+% *******************
+
+listSum([], Counter, Counter).
+listSum([Age|Ages], CounterAux, CounterFinal):-
+        Aux is CounterAux + Age,
+        listSum(Ages, Aux, CounterFinal).
+averageAge(Game, AverageAge):-
+        findall(Age,(played(Username, Game, _, _), player(_, Username, Age)), List),
+        listSum(List, 0, AgeSum),
+        length(List, Num),
+        AverageAge is AgeSum/Num.
+
+% *******************
+% * Pergunta 9 - V1 *
+% *******************
+
+% ********************
+% * Pergunta 10 - V1 *
+% ********************
